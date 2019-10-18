ca8f0e874b38a0efdb0f8a3621d7f0da1f28daa2
Santi Espinosa <saigesp@gmail.com>
2019-10-18 22:21:39 +0200

    basic map

--- src/components/contact-page.vue
+++ src/components/contact-page.vue
@@ -5,6 +5,13 @@
       <section class="page__section page__section--head">
         <h1 class="page__title">Contacto</h1>
       </section>
+
+      <section class="page__section page__section--contents">
+        <h4>Federación Regional de Asociaciones Vecinales de Madrid</h4>
+        <p><a href="https://aavvmadrid.org/">aavvmadrid.org</a><br>fravm@aavvmadrid.org<br>C/ Camino de Vinateros, 53 bis, 1º izda. 28030, Madrid<br>+34 91 725 29 09</p>
+        <h4>Dataira</h4>
+        <p><a href="https://www.dataira.com/">dataira.com</a><br>hola@dataira.com<br>Av del Mediterráneo, 45, 28007, Madrid<br>+34 610 33 99 57</p>
+      </section>
       
     </div>
   </div>
